8ccf1b2b2994248bb1c9c20ed9ce38a0b1678a5b
deepanshu17 <dimps1796@gmail.com>
2016-09-23 21:24:50 +0530

    Prob1 updates

--- prob1.m
+++ prob1.m
@@ -1,8 +1,51 @@
-%% Window detection using Hough Transforms %%
-sigma=1;
-I = imread('building_1.jpg');
-gray_I = rgb2gray(I);
-[g3, t3]=edge(gray_I, 'canny', [0.04 0.10], sigma);
+%% Football player detection %%
+addpath('Positive');
+addpath('Negetive');
+%% Read the image and resize to save computation time %%
+D_negetive = dir('Negetive/*.jpg');
+D_positive = dir('Positive/*.jpg');
+D_test = dir('Test/*.jpg');
+positive_features = [];
+negetive_features = [];
 
-H=hough(gray_I);
-imshow(H,[])
+pos_labels = ones(size(D_positive,1),1);
+neg_labels = zeros(size(D_negetive,1),1);
+
+%find the HOG descriptor value for all images 
+for i=1:size(D_positive,1)
+	filename = ['Positive/',D_positive(i).name];
+	im = imread(filename);
+	im = imresize(im,[128 64]);
+	positive_features =  [positive_features;extractHOGFeatures(im,'CellSize',[4 4], 'BlockSize',[8 8])];
+	
+	n_filename = ['Negetive/',D_negetive(i).name];
+	n_im = imread(n_filename);
+	n_im = imresize(n_im,[128 64]);
+	negetive_features =  [negetive_features;extractHOGFeatures(n_im,'CellSize',[4 4], 'BlockSize',[8 8])];
+end
+labels = [pos_labels;neg_labels];
+features = [positive_features;negetive_features];
+
+[svmStructure] = svmtrain(features, labels);
+
+test_images = [];
+test_features = [];
+testimg=imread('soccer_1.png');
+i=1;
+no_of_players=0;
+h=imshow(testimg);
+while i+128<size(testimg,1)
+	j=1;
+	while j+64<size(testimg,2)
+		imgWindow = testimg(i:i+128,j:j+64,:);
+		test_features =  extractHOGFeatures(imgWindow,'CellSize',[4 4], 'BlockSize',[8 8]);
+		predictClass = svmclassify(svmStructure, test_features);
+		if predictClass==1
+			no_of_players=no_of_players+1;
+			h2=imrect(gca,[j i 64 128]);
+		end
+		j=j+64;
+	end
+	i=i+128;
+end
+disp(no_of_players)
--- prob2.m
+++ prob2.m
@@ -1,39 +1,20 @@
-%% Football player detection %%
-addpath('Positive');
-addpath('Negetive');
-%% Read the image and resize to save computation time %%
-D_negetive = dir('Negetive/*.jpg');
-D_positive = dir('Positive/*.jpg');
-D_test = dir('Test/*.png');
-positive_features = [];
-negetive_features = [];
+%% Window detection using Hough Transforms %%
+%% Hough transf function used : Detect lines in grayscale image using Hough Transform by Tao Peng %%
+addpath('Prob2');
+I = imread('building_1.jpg');
+[accum, axis_rho, axis_theta, lineprm, lineseg] = Hough_Grd(rgb2gray(I), 6, 0.02);
+figure; imagesc(I); colormap('gray'); axis image;
+DrawLines_2Ends(lineseg);
+title('Image with Line Segments Detected');
 
-pos_labels = ones(size(D_positive,1),1);
-neg_labels = zeros(size(D_negetive,1),1);
-
-%find the HOG descriptor value for all images 
-for i=1:size(D_positive,1)
-	filename = ['Positive/',D_positive(i).name];
-	im = imread(filename);
-	im = imresize(im,[160 64]);
-	positive_features =  [positive_features;extractHOGFeatures(im,'CellSize',[4 4], 'BlockSize',[8 8])];
-	
-	n_filename = ['Negetive/',D_negetive(i).name];
-	n_im = imread(n_filename);
-	n_im = imresize(n_im,[160 64]);
-	negetive_features =  [negetive_features;extractHOGFeatures(n_im,'CellSize',[4 4], 'BlockSize',[8 8])];
+binaryImage = rgb2gray(I) < 128;
+cc = bwconncomp(binaryImage);
+measurements = regionprops(cc, 'BoundingBox');
+for k = 1 : length(measurements)
+  thisBB = measurements(k).BoundingBox;
+  if thisBB(4) <= 3  % If it's shorter than 4 lines tall.
+    message = sprintf('Blob #%d is horizontal.', k);
+    sprintf('%s\n', message);
+    uiwait(helpdlg(message));
+  end
 end
-test_images = [];
-test_features = [];
-for i=1:size(D_test,1)
-	filename = ['Test/',D_test(i).name];
-	im = imread(filename);
-	im = imresize(im,[160 64]);
-	test_images = [test_images;im];
-	test_features =  [test_features;extractHOGFeatures(im,'CellSize',[4 4], 'BlockSize',[8 8])];
-end
-labels = [pos_labels;neg_labels];
-features = [positive_features;negetive_features];
-
-[svmStructure] = svmtrain(features, labels);
-[predictClass] = svmclassify(svmStructure, test_features); 
